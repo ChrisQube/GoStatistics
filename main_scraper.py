8012469dc69110ead94518e7a54d870f2f9a8d29
ChrisQube <gameqube@outlook.com>
2022-09-07 22:38:43 +1200

    Attempt in iteration

--- main_scraper.py
+++ main_scraper.py
@@ -16,11 +16,27 @@ table = soup.find('table', id='gameslist')
 titles = [x.text for x in table.find('thead').find_all('td')]
 #print(titles)
 
-# Get all rows and store them as lists in a list.
-rows = [[x.text for x in row.find_all('td')] for row in table.find('tbody').find_all('tr')]
-#print(rows)
+df = pd.DataFrame(columns=titles)
 
-# Create the dataframe.
-df = pd.DataFrame(rows, columns=titles)
+# Get list of each URL from file
+filePath = "C:/Users/Chris/Documents/GitHub/GoStatistics/Database sample.txt"
+with open(filePath, 'r') as file:
+    lines = file.readlines()
+    urls = [line.rstrip() for line in lines]
+
+# For each URL in list capture the table --> rows
+for url in urls:
+    currentPage = requests.get(url)
+    soup = BeautifulSoup(currentPage.content, 'lxml')
+    table = soup.find('table', id='gameslist')
+    # Get all rows and store them as lists in a list.
+    rows = [[x.text for x in row.find_all('td')] for row in table.find('tbody').find_all('tr')]
+    # Append rows to the dataframe: df = pd.concat(df, pd.DataFrame(rows, columns=titles))
+    df = pd.concat(df, pd.DataFrame(rows, columns=titles))
+
+# close file
+file.close()
 
 df.head()
+
+# Export csv
